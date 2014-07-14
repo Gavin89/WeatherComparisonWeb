d5e18d0bdb908ae3bb6e20776f53c599debdde61
Gavin <gavinhardy1@hotmail.co.uk>
2014-07-14 20:15:36 +0100

    Updated port to 80

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(3000, function() {
+var server = app.listen(80, function() {
     console.log('Listening on port %d', server.address().port);
 });
