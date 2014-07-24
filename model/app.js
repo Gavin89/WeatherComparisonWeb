9e20a66abf92575ed8ddd3e29a1551022014d727
Gavin <gavinhardy1@hotmail.co.uk>
2014-07-24 23:57:03 +0100

    dsf

--- model/app.js
+++
@@ -1,7 +0,0 @@
-var http = require('http'),
-db = require('./model/db'),
-pages = require('./pages');
-
-http.createServer(function (req, res) {
-	pages.index(req, res);
-}).listen(8888, '127.0.0.1');ls -lrt
