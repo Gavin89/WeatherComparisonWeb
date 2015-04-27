caa34220bfd974308e4f7f927472f85539aabfb7
Gavin89 <hardygtw@aston.ac.uk>
2015-04-27 11:24:04 +0100

    added different values in rmse

--- public/js/main.js
+++ public/js/main.js
@@ -246,22 +246,18 @@ var search_by_location_keyword = function(location_name, callback) {
 			if(score <= 0.9) {
 				rating = 5;
 			}
-			else if(score <= 1.4) {
-				rating = 4;
-			}
-			else if(score <= 2.0) {
-				rating = 3;
-			}
-			else if(score <= 2.4) {
-				rating = 2
-			}
-			else if(score <= 4.0) {
-				rating = 1;
-			}
-			else
-			{
-				rating = 0;
-			}
+							else if(score > 0.9 && <= 1.4) {
+								rating = 4;
+							}
+							else if(score > 1.4 && <= 2.0) {
+								rating = 3;
+							}
+							else if(score > 2.0 && <= 2.4) {
+								rating = 2
+							}
+							else if(score > 2.4 && <= 4.0) {
+								rating = 1;
+							}
 			
 					
 						
@@ -501,21 +497,17 @@ var search_by_location_keyword = function(location_name, callback) {
 							if(score <= 0.9) {
 								rating = 5;
 							}
-							else if(score <= 1.4) {
+							else if(score > 0.9 && <= 1.4) {
 								rating = 4;
 							}
-							else if(score <= 2.0) {
+							else if(score > 1.4 && <= 2.0) {
 								rating = 3;
 							}
-							else if(score <= 2.4) {
+							else if(score > 2.0 && <= 2.4) {
 								rating = 2
 							}
-							else if(score <= 4.0) {
+							else if(score > 2.4 && <= 4.0) {
 								rating = 1;
-							}
-							else
-							{
-								rating = 0;
 							}
 							
 						var rating_col = $('<td/>');
@@ -654,16 +646,16 @@ var search_by_location_keyword = function(location_name, callback) {
 							if(score <= 0.9) {
 								rating = 5;
 							}
-							else if(score <= 1.4) {
+							else if(score > 0.9 && <= 1.4) {
 								rating = 4;
 							}
-							else if(score <= 2.0) {
+							else if(score > 1.4 && <= 2.0) {
 								rating = 3;
 							}
-							else if(score <= 2.4) {
+							else if(score > 2.0 && <= 2.4) {
 								rating = 2
 							}
-							else if(score <= 4.0) {
+							else if(score > 2.4 && <= 4.0) {
 								rating = 1;
 							}
 							else
